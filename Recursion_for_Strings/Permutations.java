c8c870643fbf83b372ee4e80ef3225cb21048c5b
Hrithik Arun Kumar M <128972805+Hrithik0216@users.noreply.github.com>
2023-12-28 23:01:59 +0530

    permutations final

--- Recursion_for_Strings/Permutations.java
+++ Recursion_for_Strings/Permutations.java
@@ -1,10 +1,14 @@
 package Recursion_for_Strings;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Permutations {
     public static void main(String[] args) {
-        String up = "abc";
+        String up = "ab";
         String p = "";
-        permutatePrint(p, up);
+        ArrayList<String> ans = permutateString(p, up);
+        System.out.println(ans);
     }
 
     static void permutatePrint(String p, String up) {
@@ -20,5 +24,22 @@ public class Permutations {
             String s = p.substring(i, p.length());
             permutatePrint(f + ch + s, up.substring(1));
         }
+    }
+
+    static ArrayList<String> permutateString(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+        ArrayList<String> ans = new ArrayList<>();
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0, i);
+            String s = p.substring(i, p.length());
+            ans.addAll(permutateString(f + ch + s, up.substring(1)));
+        }
+        return ans;
     }
 }
